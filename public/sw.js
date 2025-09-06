129a42d9e2afde7ae06baa763a88b1aee958d752
Britt Underwood <britt.underwood1@gmail.com>
2025-09-06 12:37:53 -0400

    First magical version of the wishlist app

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+// A simple caching spell to make your app work offline
+
+const CACHE_NAME = 'crystal-wishlist-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+  // You can add more assets here, like your icons
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
