365427159f8f03243421e7c9c137324f05c1cf6c
Britt <britt.underwood1@gmail.com>
2026-08-22 23:58:47 -0400

    Bump service worker cache version

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // A simple caching spell to make your app work offline
 
-const CACHE_NAME = 'crystal-wishlist-cache-v12';
+const CACHE_NAME = 'crystal-wishlist-cache-v13';
 const urlsToCache = [
   '/',
   '/index.html',
