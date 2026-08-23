ea67a40eb131ae9efe6db35e9ebeb8ab14c324a0
Britt <britt.underwood1@gmail.com>
2026-08-23 00:22:56 -0400

    Fix modal height overflow on mobile

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // A simple caching spell to make your app work offline
 
-const CACHE_NAME = 'crystal-wishlist-cache-v14';
+const CACHE_NAME = 'crystal-wishlist-cache-v15';
 const urlsToCache = [
   '/',
   '/index.html',
